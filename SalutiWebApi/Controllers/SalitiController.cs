c228ef6bbacec4d6d7cdbf78cede2ed179fe3d7c
Nicola La Rocca <nicola_larocca@xantrix.it>
2021-12-18 17:38:39 +0100

    Aggiungere i file di progetto.

---
+++ SalutiWebApi/Controllers/SalitiController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SalutiWebApi.Controllers
+{
+    [ApiController]
+    [Route("api/saluti")]
+    public class SalitiController
+    {
+        public string getSaluti()
+        {
+            return "Saluti, sono la tua prima web api creata in c#";
+        }
+    }
+}
